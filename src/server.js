c84b794a079c447932237e4dc26f58be683ae77b
Jacob Gakstatter <jacobgakstatter@gmail.com>
2018-02-23 10:54:06 -0500

    npm-answers endpoint working successfully

--- src/server.js
+++ src/server.js
@@ -15,46 +15,64 @@ server.use(bodyParser.json());
 
 server.get('/accepted-answer/:soID', (req, res) => {
   const { soID } = req.params;
-  Post.findOne({soID})
-  .then(id => {
-    const acceptedID = id.acceptedAnswerID;
-    Post.findOne({ soID: acceptedID })
-    .then(acceptedID => {
-      res.send(acceptedID);
+  Post.findOne({ soID })
+    .then((id) => {
+      const acceptedID = id.acceptedAnswerID;
+      Post.findOne({ soID: acceptedID }).then((acceptedAnswer) => {
+        res.send(acceptedAnswer);
+      });
     })
-  }).catch(error => {
-    res.status(500).send({ error });
-  })
-})
+    .catch((error) => {
+      res.status(500).send({ error });
+    });
+});
 
 server.get('/top-answer/:soID', (req, res) => {
   const { soID } = req.params;
-  Post.findOne({soID})
-    .then(id => {
+  Post.findOne({ soID })
+    .then((id) => {
       const acceptedID = id.acceptedAnswerID;
-      Post.find({parentID: soID}).sort({"score": -1})
-        .then(scores => {
-          if(scores[0].soID === acceptedID) {
+      Post.find({ parentID: soID })
+        .sort({ score: -1 })
+        .then((scores) => {
+          if (scores[0].soID === acceptedID) {
             res.send(scores[1]);
           } else {
             res.send(scores[0]);
           }
-        })
-    }).catch(error => {
-      res.status(500).send({ error });
+        });
     })
-})
+    .catch((error) => {
+      res.status(500).send({ error });
+    });
+});
 
 server.get('/popular-jquery-questions', (req, res) => {
-  Post.find({ "tags": "jquery" })
-    .or([{ "score": {$gt: 5000} }, { "user.reputation": {$gt: 200000} }])
-    .then(r => {
+  Post.find({ tags: 'jquery' })
+    .or([{ score: { $gt: 5000 } }, { 'user.reputation': { $gt: 200000 } }])
+    .then((r) => {
       res.send(r);
-    }).catch(error => {
-      res.status(500).send({ error: "Could not find jquery questions." });
     })
-})
+    .catch((error) => {
+      res.status(500).send({ error: 'Could not find jquery questions.' });
+    });
+});
 
+server.get('/npm-answers', (req, res) => {
+  Post.find({ tags: 'npm' })
+    .then((items) => {
+      const ids = [];
+      items.forEach((thing) => {
+        ids.push(thing.soID);
+      });
+      Post.find({ parentID: ids }).sort({ parentID: -1, score: -1 }).then((answers) => {
+        res.send(answers);
+      });
+    })
+    .catch((error) => {
+      res.status(500).send({ error });
+    });
+});
 
 // mongoose.connect('mongodb://localhost/soPosts', { useMongoClient: true })
 //   .then(each => {
